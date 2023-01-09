4a71fe78691b5997091a44ce9a51e0b1bde8c40a
Tuomas01 <tuomastheikkila@hotmail.fi>
2023-01-09 11:49:58 +0200

    starting code for the first app

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import List from './components/List';
 
 const App = () => {
   //  console.log('App starting')
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 };
 
