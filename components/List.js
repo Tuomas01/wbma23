3e18b8a9294ddc538138db0254595b4d33feb872
Tuomas01 <tuomastheikkila@hotmail.fi>
2023-01-11 11:55:26 +0200

    creates a custom hook

--- components/List.js
+++ components/List.js
@@ -1,39 +1,9 @@
-import {useEffect, useState} from 'react';
 import {FlatList, StyleSheet} from 'react-native';
-import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/ApiHooks';
 import ListItem from './ListItem';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  /* this is the same as this ^
-  const stateArray = useState([]);
-  const mediaArray = stateArray[0];
-  const setMediaArray = stateArray[1];
-  */
-
-  const loadMedia = async () => {
-    try {
-      const response = await fetch(baseUrl + 'media');
-      const allMedia = await response.json();
-      const media = await Promise.all(
-        allMedia.map(async (file) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + file.file_id);
-          return await fileResponse.json();
-        })
-      );
-
-      setMediaArray(media);
-    } catch {
-      console.log('loadMedia', error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia();
-  }, []);
-
-  console.log('testing', mediaArray);
-
+  const {mediaArray} = useMedia();
   return (
     <FlatList
       data={mediaArray}
