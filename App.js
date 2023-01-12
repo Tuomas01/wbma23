5d26cf46da483430be398885f697acf6276de9a5
Tuomas01 <tuomastheikkila@hotmail.fi>
2023-01-12 09:58:45 +0200

    adds navigation between tabs to the app

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, SafeAreaView, Platform} from 'react-native';
-import List from './components/List';
+import Navigator from './navigators/Navigator';
+import Home from './views/home';
 
 const App = () => {
   //  console.log('App starting')
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <List />
-      </SafeAreaView>
+      <Navigator />
       <StatusBar style="auto" />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-  },
-});
 
 export default App;
