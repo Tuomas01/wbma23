98bd4a84a94c4642051290df0029dfe1377e32a7
Tuomas01 <tuomastheikkila@hotmail.fi>
2023-02-06 13:06:41 +0200

    adds updateform for updating the profile to the profile page

--- views/Profile.js
+++ views/Profile.js
@@ -1,14 +1,17 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {StyleSheet, SafeAreaView, Text, Button, Image} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useTag} from '../hooks/ApiHooks';
 import {uploadsUrl} from '../utils/variables';
+import {Card, Image, Text, Button} from '@rneui/themed';
+import UpdateForm from '../components/UpdateForm';
 
 const Profile = () => {
   const {getFilesByTag} = useTag();
   const {setIsLoggedIn, user, setUser} = useContext(MainContext);
   const [avatar, setAvatar] = useState('');
+  const [toggleProfile, setToggleProfile] = useState(true);
 
   const loadAvatar = async () => {
     try {
@@ -24,44 +27,91 @@ const Profile = () => {
   }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Profile</Text>
-      <Image style={styles.image} source={{uri: uploadsUrl + avatar}} />
-      <Text style={styles.text}>Username: {user.username}</Text>
-      <Text style={styles.text}>Email: {user.email}</Text>
-      <Text style={styles.text}>Full name: {user.full_name}</Text>
-      <Button
-        title="Logout!"
-        onPress={async () => {
-          console.log('Logging out!');
-          setUser({});
-          setIsLoggedIn(false);
-          try {
-            await AsyncStorage.clear();
-          } catch (e) {
-            console.log('Clearning async storage failed', e);
-          }
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#151515',
+      }}
+    >
+      {toggleProfile ? (
+        <Card
+          containerStyle={{
+            display: 'flex',
+            alignItems: 'center',
+            width: '75%',
+            borderRadius: 20,
+          }}
+        >
+          <Text h2>Profile</Text>
+          <Image
+            source={{uri: uploadsUrl + avatar}}
+            containerStyle={{
+              aspectRatio: 1,
+              width: '100%',
+              borderWidth: 2,
+              borderColor: 'black',
+              marginBottom: 5,
+            }}
+          />
+          <Text style={{marginBottom: 5, textAlign: 'left'}}>
+            Username: {user.username}
+          </Text>
+          <Text style={{marginBottom: 5, textAlign: 'left'}}>
+            Email: {user.email}
+          </Text>
+          <Text style={{marginBottom: 5, textAlign: 'left'}}>
+            Full name: {user.full_name}
+          </Text>
+          <Button
+            title="Logout"
+            buttonStyle={{
+              borderRadius: 10,
+              backgroundColor: '#320064',
+              width: '100%',
+            }}
+            onPress={async () => {
+              console.log('Logging out!');
+              setUser({});
+              setIsLoggedIn(false);
+              try {
+                await AsyncStorage.clear();
+              } catch (e) {
+                console.log('Clearning async storage failed', e);
+              }
+            }}
+          />
+        </Card>
+      ) : (
+        <UpdateForm />
+      )}
+      <Card
+        containerStyle={{
+          display: 'flex',
+          justifyContent: 'center',
+          width: '75%',
+          borderRadius: 20,
         }}
-      />
+      >
+        <Text style={{marginBottom: 5, textAlign: 'center'}}>
+          {toggleProfile
+            ? 'Update your profile.'
+            : 'Get back to the profile page.'}
+        </Text>
+        <Button
+          title={toggleProfile ? 'Update' : 'Back to profile'}
+          buttonStyle={{
+            borderRadius: 10,
+            backgroundColor: '#320064',
+          }}
+          onPress={() => {
+            setToggleProfile(!toggleProfile);
+          }}
+        />
+      </Card>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#121212',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 40,
-  },
-  image: {
-    width: 200,
-    height: 300,
-  },
-  text: {
-    color: 'white',
-  },
-});
 
 export default Profile;
